f3bd7a50b5e21f4d9dc23fdc28917f7d6ae79c1f
Eze Ernest Chibueze <ezechibuezeernest@gmail.com>
2024-10-12 08:48:27 +0100

    check success

--- src/Components/SideNews.js
+++ src/Components/SideNews.js
@@ -14,11 +14,13 @@ export class SideNews extends Component {
 
   async componentDidMount() {
     if (this.props.news) {
-      const url = `https://newsapi.org/v2/${this.props.news.type}?${this.props.news.query}&apiKey=2768fa544ee847caac2f4ff23a08d333;
-
+      
+      const url = "https://newsapi.org/v2/everything?q=tesla&from=2024-09-12&sortBy=publishedAt&apiKey=2768fa544ee847caac2f4ff23a08d333";
+      
   
       try {
         const response = await fetch(url);
+        console.log(url)
         const data = await response.json();
         this.setState({ sidenews: data.articles });
       } catch (error) {
